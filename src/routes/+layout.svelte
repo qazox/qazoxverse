4c815da74a3db258b2d1f8d5f1d0460c00f04c16
tdgmdev <151087186+qazox@users.noreply.github.com>
2023-12-03 00:11:45 -0500

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<slot />
+
+<style>
+    :global(:root) {
+        font-family: 'Noto Sans', sans-serif;
+        
+        --gray-1: rgb(36, 43, 51);
+        --gray-2: rgb(38, 49, 85);
+        --gray-3: rgb(48, 59, 158);
+        --gray-4: rgb(106, 56, 223);
+        --gray-5: rgb(184, 129, 235);
+        --gray-6: rgb(243, 208, 247);
+    }
+
+    :global(body) {
+        padding: 0;
+        margin: 0;
+
+        background: var(--gray-1);
+        color: var(--gray-6);
+    }
+
+</style>
